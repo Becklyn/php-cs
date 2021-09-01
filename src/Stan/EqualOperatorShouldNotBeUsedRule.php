2e3bca03848820cc48eb612e4118421edf083247
Marko Vujnovic <mv@becklyn.com>
2021-09-01 16:54:28 +0200

    Moved restriction on using the == operator from CS Fixer to Stan (where it can be ignored in warranted cases)

---
+++ src/Stan/EqualOperatorShouldNotBeUsedRule.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Becklyn\PhpCs\Stan;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\Equal;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @author Marko Vujnovic <mv@becklyn.com>
+ * @since  2021-09-01
+ */
+class EqualOperatorShouldNotBeUsedRule implements Rule
+{
+
+    public function getNodeType(): string
+    {
+        return Equal::class;
+    }
+
+    public function processNode(Node $node, Scope $scope): array
+    {
+        return [
+            RuleErrorBuilder::message("The 'equal' operator ('==') should only be used in special cases like comparing arrays or data transfer objects for their values. Otherwise, the 'identical' operator ('===') should be used instead.")->build(),
+        ];
+    }
+}
